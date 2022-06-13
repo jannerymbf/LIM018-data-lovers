f5266f04915e4e1d50db1cd4e4a320090bc7e74a
Jannery Briceño <jannerymai@gmail.com>
2022-06-13 10:17:15 -0500

    Creación de función showData()

--- src/data.js
+++ src/data.js
@@ -1,7 +1,26 @@
 // estas funciones son de ejemplo
+import harryPotterData from './data/harrypotter/data.js';
 
-export const example = () => {
-  return 'example';
+export const showData = (container) => {
+
+  //1. Con el primer for voy a recorrer las keys del objeto harryPotterData
+  //2. 'j' representa un key de harryPotterData
+  // Importante: for...in solo acepta la notación de corchetes de los objetos
+  for(let j in harryPotterData){
+    let keyObjectHarry=harryPotterData[j];
+
+    // 1. Con el segundo for voy a recorrer del arreglo de cada key
+    // 2. El 'i' representa la posición de un elemento del array de cada key
+    for(let i=0; i<keyObjectHarry.length; i++){
+      let newDiv = document.createElement("div");
+      let newDivText = document.createTextNode(keyObjectHarry[i].name); //Idea: Para los elementos que no tienen name, puedo utilizar 'hasOwnProperty'
+  
+      newDiv.appendChild(newDivText);
+      container.appendChild(newDiv);
+    }
+  }
+
+  return 'showData';
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
+//Interactuar con el DOM
+import {showData} from './data.js';
+//import harryPotterData from './data/harrypotter/data.js';
+//import {characters, funFacts, spells, potions} from 'src/data/harrypotter/data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let btnShowCharacters=document.getElementById("characters");
+let spaceToShowData=document.getElementById("containerDataHarry");
+
+btnShowCharacters.addEventListener("click", ()=>{
+    showData(spaceToShowData);
+})
+
+//console.log(example, data);
