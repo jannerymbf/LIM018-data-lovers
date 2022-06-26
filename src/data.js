d2e75863815e6d8f9b792ffbdc860b1ce31a9011
Katherine Milagros <katherinebemol17@gmail.com>
2022-06-26 18:16:53 -0500

    Primera parte función computeStats

--- src/data.js
+++ src/data.js
@@ -1,35 +1,145 @@
-// import data from './data/lol/lol.js';
+/* eslint-disable no-prototype-builtins */
+// estas funciones son de ejemplo
 import harryPotterData from './data/harrypotter/data.js';
 
-// función get names
-
-export const getNames = (category) =>{
+//Función getNames
+export const getNames = (category) => {
   let categoryArray=[];
 
-  for (let i in harryPotterData){// voy a recorrer los indices del obj HP
-    let keyHarryData = harryPotterData[i]; //array de propiedades de mi obj
+  //la 'i' representa los keys (propiedades) de la data de HP - 4categorias
+  for(let i in harryPotterData){ //voy a recorrer los índices de las 4 categorías del obj HP
+    let keyHarryData=harryPotterData[i]; //esto es un array de las propiedades (categorías) de mi obj
     //console.log(keyHarryData);
-
-    if(category==[i]){ //si mi array es = a 1 propiedad de mi obj que pase
-     for(let j=0 ; j<keyHarryData.length ; j++){
-        if (keyHarryData[j].hasOwnProperty("name")){ //estoy en el array de personajes
-         categoryArray.push(keyHarryData[j].name);
-        }
-        else if (keyHarryData[j].hasOwnProperty("type")){
-         categoryArray.push(keyHarryData[j].type);
-        }
-        else if (keyHarryData[j].hasOwnProperty("title")){
-         categoryArray.push(keyHarryData[j].title)
+    //console.log(harryPotterData);
+    
+    if(category===i){ //le digo a mi categoría que sí es = a 1 índices de mi obj pase
+      for(let j=0; j<keyHarryData.length; j++){
+        if(keyHarryData[j].hasOwnProperty("name")){//estoy en el array de los personajes
+          categoryArray.push(keyHarryData[j].name); //al array se le agregue ese valor
+        }else if(keyHarryData[j].hasOwnProperty("type")){
+          categoryArray.push(keyHarryData[j].type);
+        }else if(keyHarryData[j].hasOwnProperty("title")){
+          categoryArray.push(keyHarryData[j].title);
         }
       }
-    } 
-  } 
+    }
+  }
   return categoryArray;
+
 };
 
-export const sortData = (data,sortOrder) =>{
- 
+//Función sortData
+export const sortData = (data, sortOrder) => {
 
+  if(sortOrder=="AZ"){
+    data.sort();
+  }else{
+    data.sort(function compare(a,b){
+      if(b>a){
+        return 1;
+      }else if(a>b){
+        return -1;
+      }else if(a==b){
+        return 0;
+      }
+    })
+  }
+  return data;
+}
+
+//Función filterDataHouse
+export const filterDataHouse = (data,condition) => {
+  let arrayDataWithCondition;
+  let namesHouseArray=[];
+  
+  arrayDataWithCondition=data.filter(obj => {
+    return obj.house==condition;
+  })
+  arrayDataWithCondition.forEach(obj => {
+    namesHouseArray.push(obj.name);
+  })
+  return namesHouseArray; //Nos arroja un array con los nombres de los personajes según la casa
+}
+
+//Función filterDataBook
+export const filterDataBook = (data,condition) => {
+  let arrayDataWithCondition=[];
+
+  data.forEach(obj => {
+    obj.books_featured_in.forEach(number => {
+      if(number==condition){
+        arrayDataWithCondition.push(obj.name);
+      }
+    })   
+  })
+  return arrayDataWithCondition;
+}
+
+//Función computeStatsHouse
+export const computeStatsHouse = (data,house) => {
+  let numberCharactersPerHouse=0;
+
+  data.forEach(obj => {
+    if (obj.house==house){
+      numberCharactersPerHouse++;
+    }
+  })
+  return numberCharactersPerHouse;
+}
+
+//Función computeStatsBook
+export const computeStatsBook = (data,book) => {
+  let numberCharactersPerBook=0;
+
+  data.forEach(obj => {
+    obj.books_featured_in.forEach(number => {
+      if(number==book){
+        numberCharactersPerBook++
+      }
+    })   
+  })
+  return numberCharactersPerBook;
+}
+
+//Función computeStatsCharacterBook
+export const computeStatsCharacterBook = (data) => {
+  let numberCharacterAllBook=0;
+  
+  data.forEach(obj => {
+    if(obj.books_featured_in.includes(1) && obj.books_featured_in.includes(2) && obj.books_featured_in.includes(3)&& obj.books_featured_in.includes(4) && obj.books_featured_in.includes(5) && obj.books_featured_in.includes(6) && obj.books_featured_in.includes(7)){
+      numberCharacterAllBook++;
+    }
+  })
+  return numberCharacterAllBook;
 }
 
 
+
+
+
+
+/*export const showData = (container) => {
+  //1. Con el primer for voy a recorrer las keys del objeto harryPotterData
+  //2. 'j' representa un key de harryPotterData
+  // Importante: for...in solo acepta la notación de corchetes de los objetos
+  for(let j in harryPotterData){
+    let keyObjectHarry=harryPotterData[j];
+    // 1. Con el segundo for voy a recorrer del arreglo de cada key
+    // 2. El 'i' representa la posición de un elemento del array de cada key
+    for(let i=0; i<keyObjectHarry.length; i++){
+      let newDiv = document.createElement("div");
+      let newDivText = document.createTextNode(keyObjectHarry[i].name); //Idea: Para los elementos que no tienen name, puedo utilizar 'hasOwnProperty'
+  
+      newDiv.appendChild(newDivText);
+      container.appendChild(newDiv);
+    }
+  }
+  return 'showData';
+};*/
+
+//En vez de showData() --> getNames() 
+//Que la función getNames() retorne un array
+
+/*export const showDetails = (element) => {
+  
+};*/
--- src/main.js
+++ src/main.js
@@ -1,72 +1,312 @@
+//Interactuar con el DOM
+import {computeStatsCharacterBook, computeStatsBook, computeStatsHouse, filterDataBook, filterDataHouse, getNames, sortData} from './data.js';
 import harryPotterData from './data/harrypotter/data.js';
-import {getNames} from './data.js';
 
-let btnShowCharacters=document.getElementById("characters"); //los llamos por su id
-let btnShowSpells=document.getElementById("spells");
+//Variables para jalar las categorías de la barra de navegación
+let btnShowCharacters=document.getElementById("characters");
 let btnShowPotions=document.getElementById("potions");
+let btnShowSpells=document.getElementById("spells");
 let btnShowBooks=document.getElementById("books");
-let spaceToShowData=document.getElementById("containerDataHarry");
 
-btnShowCharacters.addEventListener("click", ()=> {
-  spaceToShowData.innerHTML="";
-  let arrayCharacters = getNames("characters");
+//Variable para jalar la lista de datos de la data (divs creados)
+let spaceToShowData=document.getElementById("containerDataHP");
 
-  for (let i=0 ; i< arrayCharacters.length ; i++){
-    let newDiv = document.createElement("div");
-    let newDivText = document.createTextNode(arrayCharacters[i]);
+//Variables para jalar la columna donde se podran los datos de cada categoría
+let nameSpace=document.getElementById("detailName");
+let speciesSpace=document.getElementById("detailSpecies");
+let genderSpace=document.getElementById("detailGender");
+let birthSpace=document.getElementById("detailBirth");
+let ancestrySpace=document.getElementById("detailAncestry");
+let houseSpace=document.getElementById("detailHouse");
 
-    newDiv.appendChild(newDivText);
-    spaceToShowData.appendChild(newDiv);
-  }
-});
+//Variable para jalar la columna de detalles
+let detailColumn=document.querySelectorAll(".detailTable"); // Para darle ancho
 
-btnShowSpells.addEventListener("click", ()=> {
-  spaceToShowData.innerHTML="";
-  let arrayCharacters = getNames("spells");
+//Variables para mostrar las cajitas para filtrar
+let boxAlphabet=document.getElementById("selectAlphabet");
+let boxHouse=document.getElementById("boxSelectHouse");
+let boxBook=document.getElementById("boxSelectBook");
 
-  for (let i=0 ; i< arrayCharacters.length ; i++){
-    let newDiv = document.createElement("div");
-    let newDivText = document.createTextNode(arrayCharacters[i]);
+//Variables para jalar la foto que va al costado de la cajita de detalles
+let pictureCategory=document.getElementById("pictureCategory");
 
-    newDiv.appendChild(newDivText);
-    spaceToShowData.appendChild(newDiv);
-  }
-});
+//IMPORTANTE Muestra la data con su respectiva categoría
+const harryDataCharacters=harryPotterData.characters;
+const harryDataSpells=harryPotterData.spells;
+const harryDataPotions=harryPotterData.potions;
+const harryDataBooks=harryPotterData.books;
 
-btnShowPotions.addEventListener("click", ()=> {
-  spaceToShowData.innerHTML="";
-  let arrayCharacters = getNames("potions");
+//Variables para jalar las filas de la cajita de detalles
+let genderRow=document.getElementById("genderRow");
+let birthRow=document.getElementById("birthRow");
+let ancestryRow=document.getElementById("ancestryRow");
+let houseRow=document.getElementById("houseRow");
 
-  for (let i=0 ; i< arrayCharacters.length ; i++){
-    let newDiv = document.createElement("div");
-    let newDivText = document.createTextNode(arrayCharacters[i]);
-
-    newDiv.appendChild(newDivText);
-    spaceToShowData.appendChild(newDiv);
-  }
-});
+let titleSpecies2=document.getElementById("titleSpecies");
 
 
-btnShowBooks.addEventListener("click", ()=> {
-  spaceToShowData.innerHTML="";
-  let arrayCharacters = getNames("books");
+//Bonton de inicio - EXPLORE
+let btnExplore=document.querySelector(".exploreBtn");
 
-  for (let i=0 ; i< arrayCharacters.length ; i++){
-    let newDiv = document.createElement("div");
-    let newDivText = document.createTextNode(arrayCharacters[i]);
-
-    newDiv.appendChild(newDivText);
-    spaceToShowData.appendChild(newDiv);
-  }
-});
-
-spaceToShowData.addEventListener('click', (event)=> {
-console.log(event.target.innerText);
-//console.log(harryPotterData.characters);
-harryPotterData.characters.forEach (character => {
-  if(character.name === event.target.innerText){
-    console.log(character);
-  }
-
+btnExplore.addEventListener("click", () => {
+  document.querySelector(".welcomePage").style.display="none";
+  document.querySelector(".mainContent").style.display="inherit";
 })
+
+//Función para crear Divs automáticamente
+function createDivs(arrayCategory){
+  for(let i=0; i<arrayCategory.length; i++){
+    let newDiv=document.createElement("div");
+    let newDivText=document.createTextNode(arrayCategory[i]);
+
+    newDiv.appendChild(newDivText);//agregar el newDivText al newDiv
+    //newDiv.setAttribute("id",i);
+    spaceToShowData.appendChild(newDiv);//agregar el newDiv al DIV padre (containerDataHP)
+  }
+}
+
+//Evento para mostar los detalles de cada dato
+spaceToShowData.addEventListener("click", (event)=>{
+  harryDataCharacters.forEach(character => { //¿?
+    if(character.name === event.target.innerText){
+      nameSpace.innerHTML=character.name;
+      speciesSpace.innerHTML=character.species;
+      genderSpace.innerHTML=character.gender;
+      birthSpace.innerHTML=character.birth;
+      ancestrySpace.innerHTML=character.ancestry;
+      houseSpace.innerHTML=character.house;
+    }
+  })
+  harryDataSpells.forEach(spell => {
+    if(spell.name === event.target.innerText){
+      nameSpace.innerHTML=spell.name;
+      speciesSpace.innerHTML=spell.description;
+    }
+  })
+  harryDataPotions.forEach(potion => {
+    if(potion.name === event.target.innerText){
+      nameSpace.innerHTML=potion.name;
+      speciesSpace.innerHTML=potion.description;
+    }
+  })
+  harryDataBooks.forEach(book => {
+    if(book.title === event.target.innerText){
+      nameSpace.innerHTML=book.title;
+      speciesSpace.innerHTML=book.description;
+    }
+  })
 })
+
+
+//Función para pintar datos en la primera carga
+function firstLoad(){
+  const arrayCharacters=getNames("characters");
+  
+  spaceToShowData.innerHTML="";
+  createDivs(arrayCharacters);
+
+  harryDataCharacters.forEach(character => { //en la 1ra caraga q muestre la data de HP
+      if(character.name === "Harry Potter"){
+        nameSpace.innerHTML=character.name;
+        speciesSpace.innerHTML=character.species;
+        genderSpace.innerHTML=character.gender;
+        birthSpace.innerHTML=character.birth;
+        ancestrySpace.innerHTML=character.ancestry;
+        houseSpace.innerHTML=character.house;
+      }
+  })
+
+  btnShowCharacters.classList.add("navCategory"); //no lo tenemos en el HTML, se agrego en el CSS directamente
+
+  boxAlphabet.addEventListener("change", ()=>{  //cajita filtra A-Z
+    spaceToShowData.innerHTML="";
+    createDivs(sortData(arrayCharacters, boxAlphabet.value));
+  }); 
+
+  boxHouse.addEventListener("change", ()=>{
+    spaceToShowData.innerHTML="";
+    let namesHouse=filterDataHouse(harryDataCharacters,boxHouse.value);
+    createDivs(namesHouse);
+  })
+
+  boxBook.addEventListener("change", ()=>{
+    spaceToShowData.innerHTML="";
+    let namesBook=filterDataBook(harryDataCharacters,boxBook.value);
+    createDivs(namesBook);
+  })
+}
+firstLoad();
+
+//comentario ¿? PERSONAJES
+btnShowCharacters.addEventListener("click", ()=>{
+  const arrayCharacters=getNames("characters");
+
+  detailColumn.innerHTML=""; //<-- esto no está funcionando
+  pictureCategory.setAttribute("src", "pictures/wizard-hat_1.png");
+
+  //Aquí ponemos las categorías en color negrito cuando son seleccionadas
+  btnShowCharacters.classList.add("navCategory");
+  btnShowSpells.classList.remove("navCategory");
+  btnShowPotions.classList.remove("navCategory");
+  btnShowBooks.classList.remove("navCategory");
+
+  //comentario ¿?
+  boxHouse.style.visibility="visible";
+  boxBook.style.visibility="visible";
+  genderRow.style.display="table-row";
+  birthRow.style.display="table-row";
+  ancestryRow.style.display="table-row";
+  houseRow.style.display="table-row";
+  titleSpecies2.innerHTML="Species :"; //como lo explico ¿?
+
+  spaceToShowData.innerHTML="";
+  createDivs(arrayCharacters);
+
+  boxAlphabet.addEventListener("change", ()=>{ 
+    spaceToShowData.innerHTML="";
+    createDivs(sortData(arrayCharacters, boxAlphabet.value));
+  });
+  
+  boxHouse.addEventListener("change", ()=>{
+    spaceToShowData.innerHTML="";
+
+    let namesHouse=filterDataHouse(harryDataCharacters,boxHouse.value);
+    createDivs(namesHouse);
+  })
+
+  boxBook.addEventListener("change", ()=>{
+    spaceToShowData.innerHTML="";
+
+    let namesBook=filterDataBook(harryDataCharacters,boxBook.value);
+    createDivs(namesBook);
+  })
+  
+});
+
+//La sgt función oculta los elementos extra que solo son necesarios en Characters
+function hideData(){
+  boxHouse.style.visibility="hidden";
+  boxBook.style.visibility="hidden";
+  genderRow.style.display="none";
+  birthRow.style.display="none";
+  ancestryRow.style.display="none";
+  houseRow.style.display="none";
+  titleSpecies2.innerHTML="Description :";
+}
+
+//comentario ¿? HECHIZOS
+btnShowSpells.addEventListener("click", ()=>{
+  let arraySpells=getNames("spells");
+
+  detailColumn.innerHTML="";
+  pictureCategory.setAttribute("src", "pictures/magic-wand_1.png");
+
+  btnShowSpells.classList.add("navCategory");
+  btnShowCharacters.classList.remove("navCategory");
+  btnShowPotions.classList.remove("navCategory");
+  btnShowBooks.classList.remove("navCategory");
+
+  hideData();
+
+  spaceToShowData.innerHTML="";
+  createDivs(arraySpells);
+
+  boxAlphabet.addEventListener("change", ()=>{ 
+    spaceToShowData.innerHTML="";
+    createDivs(sortData(arraySpells, boxAlphabet.value));
+  }); 
+});
+
+
+//comentario ¿? POCIONES
+btnShowPotions.addEventListener("click", ()=>{
+  let arrayPotions=getNames("potions");
+
+  detailColumn.innerHTML="";
+  pictureCategory.setAttribute("src", "pictures/cauldron_1.png");
+
+  btnShowPotions.classList.add("navCategory");
+  btnShowCharacters.classList.remove("navCategory");
+  btnShowSpells.classList.remove("navCategory");
+  btnShowBooks.classList.remove("navCategory");
+
+  hideData();
+
+  spaceToShowData.innerHTML="";
+  createDivs(arrayPotions);
+
+  boxAlphabet.addEventListener("change", ()=>{ 
+    spaceToShowData.innerHTML="";
+    createDivs(sortData(arrayPotions, boxAlphabet.value));
+  }); 
+});
+
+//comentario ¿? BOOK
+btnShowBooks.addEventListener("click", ()=>{
+  let arrayBooks=getNames("books");
+
+  detailColumn.innerHTML="";
+  pictureCategory.setAttribute("src", "pictures/open-book_1.png");
+
+  btnShowBooks.classList.add("navCategory");
+  btnShowCharacters.classList.remove("navCategory");
+  btnShowPotions.classList.remove("navCategory");
+  btnShowSpells.classList.remove("navCategory");
+
+  hideData();
+
+  spaceToShowData.innerHTML="";
+  createDivs(arrayBooks);
+
+  boxAlphabet.addEventListener("change", ()=>{ 
+    spaceToShowData.innerHTML="";
+    createDivs(sortData(arrayBooks, boxAlphabet.value));
+  }); 
+});
+
+//Variables para trabajar ComputeStatsHouse()
+let statsGryff=document.getElementById("gryffNumber");
+let statsSlyt=document.getElementById("slytNumber");
+let statsRaven=document.getElementById("ravenNumber");
+let statsHuffle=document.getElementById("huffleNumber");
+
+//La sgt función llama a computeStats para mostrar los personajes por casa
+function statsCharactersPerHouse(){
+  statsGryff.innerHTML=computeStatsHouse(harryDataCharacters,"Gryffindor");
+  statsSlyt.innerHTML=computeStatsHouse(harryDataCharacters,"Slytherin");
+  statsRaven.innerHTML=computeStatsHouse(harryDataCharacters,"Ravenclaw");
+  statsHuffle.innerHTML=computeStatsHouse(harryDataCharacters,"Hufflepuff");
+}
+statsCharactersPerHouse();
+
+//Variables para trabajar computeStatsBook()
+let statsBook1=document.getElementById("book1Number");
+let statsBook2=document.getElementById("book2Number");
+let statsBook3=document.getElementById("book3Number");
+let statsBook4=document.getElementById("book4Number");
+let statsBook5=document.getElementById("book5Number");
+let statsBook6=document.getElementById("book6Number");
+let statsBook7=document.getElementById("book7Number");
+
+//La sgt función llama a computeStats para mostrar los personajes por libro
+function statsCharactersPerBook(){
+  statsBook1.innerHTML=computeStatsBook(harryDataCharacters,1);
+  statsBook2.innerHTML=computeStatsBook(harryDataCharacters,2);
+  statsBook3.innerHTML=computeStatsBook(harryDataCharacters,3);
+  statsBook4.innerHTML=computeStatsBook(harryDataCharacters,4);
+  statsBook5.innerHTML=computeStatsBook(harryDataCharacters,5);
+  statsBook6.innerHTML=computeStatsBook(harryDataCharacters,6);
+  statsBook7.innerHTML=computeStatsBook(harryDataCharacters,7);
+}
+statsCharactersPerBook();
+
+//Variable para trabajar computeStatsAllCharacterInTheBook 
+let statsAllCharacters=document.getElementById("allBooksNumber");
+
+//La sgt fucnión llama a computeStatsCharacterBook
+function statsCharactersInAllBooks(){
+  statsAllCharacters.innerHTML=computeStatsCharacterBook(harryDataCharacters);
+}
+statsCharactersInAllBooks();
+
+//console.log(example, data);
